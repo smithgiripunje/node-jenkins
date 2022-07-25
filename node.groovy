f28d034bf9c1587c610cf3c49b48a84c04cf4b3b
smithgiripunje <smithgiripunje@gmail.com>
2022-07-25 11:46:25 +0530

    modified

--- node.groovy
+++ node.groovy
@@ -1,1 +1,24 @@
-pi
+pipeline{
+    agent {
+        label = "linux"
+    }
+    stages{
+        stage("A"){
+            steps{
+                echo "========executing A========"
+            }
+            post{
+                always{
+                    echo "========always========"
+                }
+                success{
+                    echo "========A executed successfully========"
+                }
+                failure{
+                    echo "========A execution failed========"
+                }
+            }
+        }
+    }
+  
+        }
